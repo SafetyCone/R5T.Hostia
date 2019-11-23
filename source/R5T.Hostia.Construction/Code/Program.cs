076fec21deafb2c1c8e89d31c5fa2335356ca729
MinexAutomation <minex.automation@gmail.com>
2019-11-22 17:20:12 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Hostia.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Hostia.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
