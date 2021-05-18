07ab2470654c3984a239fd65c4af8eaa680c0876
David Coats <david@getsafetycone.com>
2021-05-17 22:09:55 -0700

    Add R5T.Hostia.Extensions library.

---
+++ source/R5T.Hostia.Extensions/Code/Helpers/HostBuilderHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class HostBuilderHelper
+    {
+        /// <summary>
+        /// The default host builder is nothing more than the <see cref="HostBuilder"/> default constructor.
+        /// </summary>
+        public static IHostBuilder GetDefaultHostBuilder()
+        {
+            // Nothing fancy.
+            var hostBuilder = new HostBuilder();
+            return hostBuilder;
+        }
+    }
+}
